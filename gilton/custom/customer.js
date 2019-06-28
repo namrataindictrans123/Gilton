96f2e448084eb1a0cfce7fc4858fe7b76e24e1db
namrataindictrans123 <namrata.i@indictrans.in>
2019-06-28 12:10:19 +0530

    Customer.js file

--- gilton/custom/customer.js
+++ gilton/custom/customer.js
@@ -1,10 +1,8 @@
 frappe.ui.form.on('Customer', {
 	onload: function(frm) {
-<<<<<<< HEAD
+
 		console.log("Hello world")
-=======
-		console.log("Hello world")
->>>>>>> b8d10d473ebe9390fc78bcfede615fde5821ac03
+
 	}
 
 });
