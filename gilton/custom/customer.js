ede9fb188e1051c1d3e6b94e8a74ae48e1024f04
Darpan24 <agarwal.darpan50@gmail.com>
2019-07-01 11:42:18 +0530

    [feature] currency linking[Darpan][01/07/2019]

--- gilton/custom/customer.js
+++ gilton/custom/customer.js
@@ -7,29 +7,29 @@ frappe.ui.form.on('Customer', {
 
 	validate: function(frm){
 		frm.set_value("user_name", frm.doc.email)
-	// },
+	},
 
-	// country: function(frm){
-	// 	if (frm.doc.country == "United Kingdom")
-	// 	{
-	// 		frappe.msgprint("validated")
-	// 		frm.set_value("default_currency", "GBP")
-	// 		frm.set_value("default_price_list", "Standard UK")
-	// 	}
+	country: function(frm){
+		if (frm.doc.country == "United Kingdom")
+		{
+			frappe.msgprint("validated")
+			frm.set_value("default_currency", "GBP")
+			frm.set_value("default_price_list", "Standard UK")
+		}
 
-	// 	if (frm.doc.country == "United States")
-	// 	{
-	// 		frappe.msgprint("validated for US")
-	// 		frm.set_value("default_currency", "USD")
-	// 		frm.set_value("default_price_list", "Standard US")
-	// 	}
+		if (frm.doc.country == "United States")
+		{
+			frappe.msgprint("validated for US")
+			frm.set_value("default_currency", "USD")
+			frm.set_value("default_price_list", "Standard US")
+		}
 
-	// 	if (frm.doc.country == "United Arab Emirates")
-	// 	{
-	// 		frappe.msgprint("validated for UAE")
-	// 		frm.set_value("default_currency", "ADE")
-	// 		frm.set_value("default_price_list", "Standard UAE")
-	// 	}
+		if (frm.doc.country == "United Arab Emirates")
+		{
+			frappe.msgprint("validated for UAE")
+			frm.set_value("default_currency", "ADE")
+			frm.set_value("default_price_list", "Standard UAE")
+		}
 
 	}
 
