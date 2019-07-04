a9325804662980c61bb889ba169c5eb92f11e4ca
namrataindictrans123 <namrata.i@indictrans.in>
2019-07-04 14:39:48 +0530

    [Fix][Conflicts Resolved]

--- gilton/hooks.py
+++ gilton/hooks.py
@@ -128,20 +128,12 @@ fixtures=['Custom Field','Property Setter','Role','Print Format']
 doc_events = {
 "Customer": {
  "validate":"gilton.gilton.Customization.Customer.user.create_user"
-<<<<<<< HEAD
+
  
 }
 "Sales Order" : {
 	"validate":"gilton.gilton.Customization.Sales Order.sales_order.check_availability"
 }
-}
-
-doctype_js={
-		"Customer" :["custom/customer.js"]
- 	}
-# doctype_js = {"Customer" : "custom/customer.js"}
-=======
-},
 "Delivery Note":{
 "validate":"gilton.gilton.Customization.Delivery_Note.delivery_note.submit",
 "on_submit":"gilton.gilton.Customization.Delivery_Note.delivery_note.on_submit"
@@ -158,4 +150,4 @@ doctype_js = {
 		"Quotation":["custom/quotation.js"],
         "Delivery Note":["custom/deliverynote.js"]
 	}
->>>>>>> 25c62cbd0cad1a36809530ebdef4342bff63fb40
+
